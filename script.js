1aa0029fd6b873399d007e2d25b1286c0ffb2537
Brij <7288brijkantyadav@gmail.com>
2024-07-03 09:21:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onscroll = function() {
+    const header = document.querySelector('header');
+    const sticky = header.offsetTop;
+
+    if (window.pageYOffset > sticky) {
+        header.classList.add('sticky');
+    } else {
+        header.classList.remove('sticky');
+    }
+
+
+};
